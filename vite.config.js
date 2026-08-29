feaaef5156362f04015051ae916d223717af89d8
sanjeevsenapati <sanjeev@therasoftonline.com>
2026-08-29 10:29:33 +0530

    Configure 100% single standalone HTML file site build with vite-plugin-singlefile

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000,
+  },
   server: {
     port: 3000,
     host: '0.0.0.0'
